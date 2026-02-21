05507d4545ae41ad0b422e7bc9fbddb0abe39237
Ardit Zubaku <zubaku92@gmail.com>
2026-02-21 04:11:41 +0100

    first GQL query

--- src/server.js
+++ src/server.js
@@ -4,14 +4,27 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 const PORT = 4000;
 
 const typeDefs = `
+  type Book {
+    id: ID!
+    title: String!
+    author: String!
+  }
+
   type Query {
     hello: String!
+    books: [Book!]!
   }
 `
+
+const books = [
+  { id: '1', title: 'Title 1', author: 'Author 1' },
+  { id: '2', title: 'Title 2', author: 'Author 2' }
+]
 
 const resolvers = {
   Query: {
     hello: () => "Hello, GraphQL",
+    books: () => books,
   }
 }
 
