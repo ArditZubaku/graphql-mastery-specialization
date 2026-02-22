2dd44bbf7a0d83d8484bbb83618a5cd69b5657de
Ardit Zubaku <zubaku92@gmail.com>
2026-02-22 01:31:56 +0100

    create react client and wrapp App with ApolloProvider + apolloClient

---
+++ client/src/apolloClient.js
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: "http://localhost:4000/" });
+
+const client = new ApolloClient({
+  cache: cache,
+  link: link,
+});
+
+export default client;
