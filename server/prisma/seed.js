e7f1ecdb3338466367ab442f7516e1666fa79a2d
Ardit Zubaku <zubaku92@gmail.com>
2026-02-26 01:21:47 +0100

    seed db

---
+++ server/prisma/seed.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client"
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+
+const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" })
+const prisma = new PrismaClient({
+  adapter,
+  errorFormat: "pretty",
+  log: ["info", "query", "error"],
+  omit: {
+    user: {
+      gender: true
+    }
+  }
+});
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      { id: "11", email: "user11@email.com", gender: "MALE" },
+      { id: "12", email: "user12@email.com", gender: "FEMALE" },
+    ]
+  })
+
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main().catch(console.error).finally(() => prisma.$disconnect())
